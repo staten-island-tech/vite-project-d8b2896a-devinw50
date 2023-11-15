ea058e39eb8337a3ffb33485074cfdb68977a7f3
devinw50 <addicteddmg@gmail.com>
2023-11-15 10:15:25 -0500

    we
    ew

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,3 +1,60 @@
-const menu = ["[hi, bye"]
-const name = ["dan, bon"]
-export {menu, name}
+const menu = [
+    {
+        name: "McChicken",
+        calories: "357",
+        img: "",
+        price: "$2.79",
+
+},
+{
+    name: "Cheeseburger",
+    calories: "313",
+    img: "",
+    price: "$2.79",
+    
+},
+{
+    name: "Hamburger",
+    calories: "250",
+    img: "",
+    price: "$2.79",
+    
+},
+{
+    name: "Diet Coke",
+    calories: "0",
+    img: "",
+    price: "$2.79",
+    
+},
+{
+    name: "Big Mac",
+    calories: "563",
+    img: "",
+    price: "$2.79",
+    
+},
+{
+    name: "Large Fries",
+    calories: "480",
+    img: "",
+    price: "$2.79",
+    
+},
+{
+    name: "Hash Brown",
+    calories: "140",
+    img: "",
+    price: "$2.49",
+    
+},
+{
+    name: "Fish-o-Filet",
+    calories: "379",
+    img: "",
+    price: "$2.79",
+    
+},
+]
+
+export {menu}
