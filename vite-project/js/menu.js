0be099f0810c612ac5fb3d3fddcd13c721179008
devinw50 <addicteddmg@gmail.com>
2023-11-17 10:09:02 -0500

    Update menu.js

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -2,59 +2,59 @@ const menu = [
     {
         name: "McChicken",
         calories: "357",
-        img: "",
+        img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/mcchicken-sandwich.webp",
         price: "$2.79",
 
 },
 {
-    name: "Cheeseburger",
-    calories: "313",
-    img: "",
-    price: "$2.79",
+    name: "Triple Cheeseburger",
+    calories: "578",
+    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/triple-cheeseburger.png",
+    price: "$2.99",
     
 },
 {
     name: "Hamburger",
     calories: "250",
-    img: "",
-    price: "$2.79",
+    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/hamburger.png",
+    price: "$1.99",
     
 },
 {
     name: "Diet Coke",
     calories: "0",
-    img: "",
-    price: "$2.79",
+    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/09/diet-coke.webp",
+    price: "$1.19",
     
 },
 {
     name: "Big Mac",
     calories: "563",
-    img: "",
-    price: "$2.79",
+    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/big-mac.png",
+    price: "$4.99",
     
 },
 {
     name: "Large Fries",
     calories: "480",
-    img: "",
-    price: "$2.79",
+    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/fries.png",
+    price: "$4.89",
     
 },
 {
     name: "Hash Brown",
     calories: "140",
-    img: "",
-    price: "$2.49",
+    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/08/hash-brown.webp",
+    price: "$1.99",
     
 },
 {
     name: "Fish-o-Filet",
     calories: "379",
-    img: "",
-    price: "$2.79",
+    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/filet-o-fish.png",
+    price: "$4.69",
     
 },
 ]
 
-export {menu}
+export { menu }
