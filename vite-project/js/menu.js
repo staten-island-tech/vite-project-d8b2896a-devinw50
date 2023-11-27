7197cdefef7811ba81fb1b7986635a725e9ddf27
devinw50 <addicteddmg@gmail.com>
2023-11-27 10:13:30 -0500

    wer
    werw

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,59 +1,67 @@
 const menu = [
     {
         name: "McChicken",
-        calories: "357",
+        calories: 357,
         img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/mcchicken-sandwich.webp",
-        price: "$2.79",
+        price: 2.79,
+        healthy: false,
 
 },
 {
     name: "Triple Cheeseburger",
-    calories: "578",
+    calories: 578,
     img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/triple-cheeseburger.png",
-    price: "$2.99",
-    
+    price: 2.99,
+    healthy: false,
+
 },
 {
     name: "Hamburger",
-    calories: "250",
+    calories: 250,
     img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/hamburger.png",
-    price: "$1.99",
-    
+    price: 1.99,
+    healthy: false,
+
 },
 {
     name: "Diet Coke",
-    calories: "0",
+    calories: 0,
     img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/09/diet-coke.webp",
-    price: "$1.19",
-    
+    price: 1.19,
+    healthy: true,
+
 },
 {
     name: "Big Mac",
-    calories: "563",
+    calories: 563,
     img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/big-mac.png",
-    price: "$4.99",
-    
+    price: 4.99,
+    healthy: false,
+
 },
 {
     name: "Large Fries",
-    calories: "480",
+    calories: 480,
     img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/fries.png",
-    price: "$4.89",
-    
+    price: 4.89,
+    healthy: false,
+
 },
 {
-    name: "Hash Brown",
-    calories: "140",
+    name: "Unsweetened Tea",
+    calories: 0,
     img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/08/hash-brown.webp",
-    price: "$1.99",
-    
+    price: 1.79,
+    healthy: true,
+
 },
 {
-    name: "Fish-o-Filet",
-    calories: "379",
+    name: "Apple slices",
+    calories: 15,
     img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/filet-o-fish.png",
-    price: "$4.69",
-    
+    price: 1.39,
+    healthy: true,
+
 },
 ]
 
