1428b67b12ff0a212d35755f864b6d3c27172b29
devinw50 <addicteddmg@gmail.com>
2023-11-28 10:11:50 -0500

    erg
    erg

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -7,3 +7,20 @@ document.querySelector(".btn").addEventListener("click", function () {
       document.body.classList.remove("warm");
     }
   });
+
+
+function insertCards(arr){
+  arr.array.forEach(food => {
+    DOMSelectors.column.insertAdjacentHTM (
+      "beforeend",
+      `<div class="column">
+      <div class="card">
+          <h2 class="card-header" id="column">${input}</h2>
+          <h3 class="card-info" id="column">Author: ${input2}</h3>
+          <img src = "${input3}" alt = "" class = "img">
+          <button type="click" class="remove">Remove</button>
+      </div>
+   `)
+    
+  });
+}
