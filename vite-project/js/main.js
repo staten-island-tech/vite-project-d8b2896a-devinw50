499c4162fd06ef695fe24ecd8d431318df865310
devinw50 <144460843+devinw50@users.noreply.github.com>
2023-11-29 00:12:29 -0500

    rt
    rtr

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,4 +1,67 @@
-import "../styles/style.css"; 
-import "../js/dom.js";
-import { menu } from "./menu";
-console.log(menu)
+import "../styles/style.css";
+import { cards } from "./menu";
+
+document.querySelector(".theme-btn").addEventListener("click", function () {
+    if (document.body.classList.contains("light")) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+  });
+
+const DOMSelectors = {
+    column: document.querySelector(".column"),
+    healthyBtn: document.querySelector('.healthy'),
+    unhealthyBtn: document.querySelector('.unhealthy'),
+    themeBtn: document.querySelector('.theme-btn'),
+    lowcalBtn: document.querySelector('.lcalories'), 
+    highcalBtn: document.querySelector('.hcalories'), 
+}
+
+function clearfields() {
+    DOMSelectors.column.innerHTML = "";
+}
+
+function insertCards(arr) {
+    arr.forEach((card) => {
+        DOMSelectors.column.insertAdjacentHTML(
+            "beforeend",
+            `<div class="card">
+                <h3 class="name">${card.name}</h3>
+                <img src="${card.img}" class="img">
+                <h4>Price: ${card.price}</h4> 
+            </div>`
+        );
+    });
+}
+insertCards(cards)
+
+
+DOMSelectors.healthyBtn.addEventListener('click', function () {
+    let newArr = cards.filter((card) => card.good === 'healthy'); 
+    clearfields();
+    insertCards(newArr);
+});
+
+DOMSelectors.unhealthyBtn.addEventListener('click', function () {
+    let newArr = cards.filter((card) => card.good === 'unhealthy'); 
+    clearfields();
+    insertCards(newArr);
+});
+
+DOMSelectors.lowcalBtn.addEventListener('click', function () {
+    let newArr = cards.filter((card) => card.calories === 'low'); 
+    clearfields();
+    insertCards(newArr);
+});
+
+DOMSelectors.highcalBtn.addEventListener('click', function () {
+    let newArr = cards.filter((card) => card.calories === 'high');
+    clearfields();
+    insertCards(newArr);
+});
+
+
+
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,68 +1,75 @@
-const menu = [
+const cards = [
     {
         name: "McChicken",
-        calories: 357,
-        img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/mcchicken-sandwich.webp",
-        price: 2.79,
-        healthy: false,
-
+        calories: "high",
+        img: "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-McChicken-Sandwich-2:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
+        price: "2.79",
+        good: "unhealthy",
+        food:"yes",
 },
 {
-    name: "Triple Cheeseburger",
-    calories: 578,
-    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/triple-cheeseburger.png",
-    price: 2.99,
-    healthy: false,
+    name: "4 McNuggets",
+    calories: "low",
+    img: "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Chicken-McNuggets-4-pieces:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
+    price: "2.99",
+    good: "unhealthy",
+    food:"yes",
 
 },
 {
     name: "Hamburger",
-    calories: 250,
-    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/hamburger.png",
-    price: 1.99,
-    healthy: false,
+    calories: "high",
+    img: "https://s7d1.scene7.com/is/image/mcdonalds/DC_202006_0001_Hamburger_Alt_832x472:1-3-product-tile-desktop?wid=765&hei=472&dpr=off",
+    price: "1.99",
+    good: "unhealthy",
+    food:"yes",
 
 },
 {
     name: "Diet Coke",
-    calories: 0,
-    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/09/diet-coke.webp",
-    price: 1.19,
-    healthy: true,
+    calories: "low",
+    img: "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-Coca-Cola-Medium-2:1-3-product-tile-desktop?wid=829&hei=515&dpr=off",
+    price: "1.19",
+    good: "healthy",
+    food:"yes",
 
 },
 {
     name: "Big Mac",
-    calories: 563,
-    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/big-mac.png",
-    price: 4.99,
-    healthy: false,
+    calories: "high",
+    img: "https://s7d1.scene7.com/is/image/mcdonalds/Header_BigMac_832x472:1-3-product-tile-desktop?wid=763&hei=472&dpr=off",
+    price: "4.99",
+    good: "unhealthy",
+    food:"yes",
 
 },
 {
-    name: "Large Fries",
-    calories: 480,
-    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/fries.png",
-    price: 4.89,
-    healthy: false,
+    name: "Small Fries",
+    calories: "high",
+    img: "https://s7d1.scene7.com/is/image/mcdonalds/DC_202002_6050_SmallFrenchFries_Standing_832x472:1-3-product-tile-desktop?wid=765&hei=472&dpr=off",
+    price: "4.89",
+    good: "unhealthy",
+    food:"yes",
 
 },
 {
-    name: "Unsweetened Tea",
-    calories: 0,
-    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/08/hash-brown.webp",
-    price: 1.79,
-    healthy: true,
+    name: "Cucumber Sticks",
+    calories: "low",
+    img: "https://evoke.ie/wp-content/uploads/2019/05/22228_MCD_UKY_CucumberSticksOverhead_2019_MR.jpg",
+    price: "1.00",
+    good: "healthy",
+    food:"yes",
 
 },
 {
     name: "Apple slices",
-    calories: 15,
-    img: "https://mcdonalds-menu.co.uk/wp-content/uploads/2022/07/filet-o-fish.png",
-    price: 1.39,
-    healthy: true,
+    calories: "low",
+    img: "https://patch.com/img/cdn/users/1586961/2012/08/raw/ef170112c47a01d09fcd891af8e9cb8d.jpg",
+    price: "1.39",
+    good: "healthy",
+    food:"yes",
 
 },
 ]
 
-export { menu }
+export { cards };
